efda2d5de6b22cb00b78c6496cca6dcaae4ab461
Rob Sutherland <rob@roberocity.com>
2011-09-22 23:37:02 -0500

    image handler library

---
+++ HttpImageHandler.cs
@@ -0,0 +1,92 @@
+﻿using System.Web;
+
+namespace Roberocity.ImageHandler
+{
+	using System;
+	using System.IO;
+
+	public static class GuidHelpers
+	{
+		public static Guid TryCreate(string g)
+		{
+			var ng = Guid.Empty;
+
+			try {
+				ng = new Guid(g);
+			}
+			catch(Exception) {
+			}
+
+			return ng;
+		}
+	}
+
+	/// <summary>
+	/// Resizes an image based on the querystring paramenters of Height and Width. 
+	/// If no parameters are given then it just returns the image. 
+	/// The image will always retain the proporitions of the original
+	/// image. The height and width of the image returned may not match the height and
+	/// width specified. The image will be scaled based on the smallest side.
+	/// </summary>
+	public class HttpImageHandler : IHttpHandler
+	{
+		public void ProcessRequest(HttpContext context)
+		{
+			context.Response.ContentType = "image/png";
+
+			var id = context.Request.QueryString["id"];
+			
+			byte[] buffer;
+
+			var imgId = GuidHelpers.TryCreate(id);
+
+			if(imgId == Guid.Empty)
+			{
+
+				id = string.IsNullOrEmpty(id) ? context.Request.PhysicalPath : context.Server.MapPath(id);
+
+				if(ShouldResizeImage(context)) {
+					int h;
+					int w;
+					int.TryParse(context.Request.QueryString["Width"], out w);
+					int.TryParse(context.Request.QueryString["Height"], out h);
+					buffer = Roberocity.ImageEditing.GetResizedImage(id, w, h);
+				}
+				else {
+					buffer = File.ReadAllBytes(id);
+				}
+			}
+			else {
+				var basePath = Path.Combine(AppDomain.CurrentDomain.BaseDirectory, @"images\pressreleases");
+				if(ShouldResizeImage(context)) {
+					int h;
+					int w;
+					int.TryParse(context.Request.QueryString["Width"], out w);
+					int.TryParse(context.Request.QueryString["Height"], out h);
+					buffer = Roberocity.ImageEditing.GetResizedImage(Path.Combine(basePath, id), w, h);
+				}
+				else {
+					Roberocity.Storage.IFileStore storage = new Roberocity.Storage.DiskFileStore(basePath);
+					using(var stream = storage.Get(imgId)) {
+						buffer = new byte[stream.Length];
+						stream.Read(buffer, 0, buffer.Length);
+					}
+				}
+			}
+			context.Response.OutputStream.Write(buffer, 0, buffer.Length);
+		}
+
+		static bool ShouldResizeImage(HttpContext context)
+		{
+			return !(string.IsNullOrEmpty(context.Request.QueryString["Width"]) && string.IsNullOrEmpty(context.Request.QueryString["Height"]));
+		}
+
+		public bool IsReusable
+		{
+			get
+			{
+				return false;
+			}
+		}
+	}
+}
---
+++ ImageEditing.cs
@@ -0,0 +1,106 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Roberocity
+{
+	using System.Drawing;
+	using System.Drawing.Imaging;
+	using System.IO;
+
+	public class ImageEditing
+	{
+		/// <summary>
+		/// Gets the resized image
+		/// </summary>
+		/// <param name="path">The path to the image to resize</param>
+		/// <param name="width">The width to size it to</param>
+		/// <param name="height">The height to size it to</param>
+		/// <returns>A byte array of image data</returns>
+		public static byte[] GetResizedImage(String path, int width, int height)
+		{
+			byte[] outBytes;
+			using(var imgIn = new Bitmap(path))
+			{
+				double y = imgIn.Height;
+				double x = imgIn.Width;
+				double factor = 1;
+				if (width > 0)
+				{
+					factor = width/x;
+				}
+				else if (height > 0)
+				{
+					factor = height/y;
+				}
+
+				var offset = 0;
+				using (var outStream = new MemoryStream())
+				{
+					using (var imgOut = new Bitmap((int) (x*factor) + offset, (int) (y*factor) + offset))
+					{
+						using (var g = Graphics.FromImage(imgOut))
+						{
+							//var colors = new List<Color>() { Color.Blue, Color.Green, Color.Red, Color.Salmon, Color.Silver, Color.Yellow, Color.Tomato, Color.Orange, Color.Pink };
+							//var d = DateTime.Now.Ticks;
+							//var r = (int)(d % colors.Count);
+							//g.Clear(colors[r]);
+                                   g.Clear(Color.White); 
+							g.InterpolationMode = System.Drawing.Drawing2D.InterpolationMode.HighQualityBilinear;
+							g.DrawImage(
+								imgIn,
+								new Rectangle(offset/2, offset/2, (int) (factor*x), (int) (factor*y)),
+								new Rectangle(0, 0, (int) x, (int) y),
+								GraphicsUnit.Pixel);
+
+							//g.DrawString(DateTime.Now.ToString("hh:mm:ss") + "\n" + g.InterpolationMode.ToString(), new Font("Arial", 11.0f), Brushes.White, new PointF(15, 20));
+							g.Flush();
+							imgOut.Save(outStream, GetImageFormat(path));
+							//throw new Exception("Testing");
+							outBytes = outStream.ToArray();
+						}
+					}
+				}
+			}
+			return outBytes;
+		}
+
+		/// <summary>
+		/// Guess the content type based on the extension 
+		/// of the original
+		/// </summary>
+		/// <param name="path"></param>
+		/// <returns></returns>
+		public static string GetContentType(String path)
+		{
+			switch(Path.GetExtension(path)) {
+				case ".bmp": return "Image/bmp";
+				case ".gif": return "Image/gif";
+				case ".jpg": return "Image/jpeg";
+				case ".png": return "Image/png";
+				default: break;
+			}
+			return "";
+		}
+
+
+		/// <summary>
+		/// Guess the image format based on the extension
+		/// of the original
+		/// </summary>
+		/// <param name="path"></param>
+		/// <returns></returns>
+		public static ImageFormat GetImageFormat(String path)
+		{
+			switch(Path.GetExtension(path)) {
+				case ".bmp": return ImageFormat.Bmp;
+				case ".gif": return ImageFormat.Gif;
+				case ".jpg": return ImageFormat.Jpeg;
+				case ".png": return ImageFormat.Png;
+				default: break;
+			}
+			return ImageFormat.Jpeg;
+		}
+	}
+}
